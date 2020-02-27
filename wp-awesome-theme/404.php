c7b3bce9e6ddba13e74dedfbf6af255edffdbd2f
alexxsanya <alexxsanya@gmail.com>
2020-02-28 02:30:34 +0300

    add template files

---
+++ wp-awesome-theme/404.php
@@ -0,0 +1,6 @@
+<?php get_header() ?>
+
+404 Template
+<p>The Content you are looking for doesn't exist</p>
+
+<?php get_footer() ?>
--- wp-awesome-theme/index.php
+++ wp-awesome-theme/index.php
@@ -1,6 +1,8 @@
 <?php get_header() ?>
 
 My Awesome Theme
+<?php get_search_form(); ?>
+
 <?php if( have_posts() ) : ?>
 	<?php while( have_posts() ) : the_post() ?>
 		<h2><a href='<?php the_permalink() ?>'><?php the_title() ?></a></h2>

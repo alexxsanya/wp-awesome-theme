b70669a73ccf99790189c8fb61fa3a7cda14ee88
alexxsanya <alexxsanya@gmail.com>
2020-03-01 23:33:20 +0300

    add pagination

--- wp-awesome-theme/category.php
+++ wp-awesome-theme/category.php
@@ -3,7 +3,20 @@
 Categories Template
 <?php get_template_part('includes/section', 'archive') ?>
 
-<?php previous_posts_link() ?>
-<?php next_posts_link() ?>
+<!-- first ofrm of paginations -->
+<!-- <?php previous_posts_link() ?>
+<?php next_posts_link() ?> -->
 
+<!-- send for of pagination -->
+<?php 
+	global $wp_query;
+
+	$big = 9999999999; // need an unlikely number
+	echo paginate_links( array(
+		'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+		'format' => '?page=%#%',
+		'current' => max(1, get_query_var('paged')),
+		'total' => $wp_query->max_num_pages,
+	));
+?>
 <?php get_footer() ?>

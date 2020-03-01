66a0f981856f5c466a6210fe7ff8a44b938972b3
alexxsanya <alexxsanya@gmail.com>
2020-03-02 00:13:42 +0300

    add image to post page

--- wp-awesome-theme/single.php
+++ wp-awesome-theme/single.php
@@ -1,5 +1,11 @@
 <?php get_header() ?>
 
+
+<?php if(has_post_thumbnail()):?>
+	Has thumbnail image
+	<img src="<?php the_post_thumbnail_url()?>" alt="<?php the_title() ?>" class='img-fluid mb-3 img-thumbnail' />
+<?php endif; ?>
+
 <?php get_template_part('includes/section', 'blogcontent') ?>
 
 <?php wp_link_pages() // to link pages with multiple page breaks?>

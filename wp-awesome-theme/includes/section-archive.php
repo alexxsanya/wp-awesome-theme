a3e2ade16ac7b7bdacd2bc256d18fc54134031f1
alexxsanya <alexxsanya@gmail.com>
2020-03-01 23:22:59 +0300

    add template part for archives & categoriess

--- wp-awesome-theme/includes/section-archive.php
+++ wp-awesome-theme/includes/section-archive.php
@@ -1,5 +1,3 @@
-<?php get_header() ?>
-
  <!-- Start the Loop. -->
  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
@@ -8,15 +6,15 @@
  	<!-- Otherwise, the div box is given the CSS class "post". -->
 
  	<?php if ( in_category( '3' ) ) : ?>
- 		<div class="post-cat-three">
+ 		<div class="card mb-3 post-cat-three">
  	<?php else : ?>
- 		<div class="post">
+ 		<div class="card mb-3 post">
  	<?php endif; ?>
 
-
+	<div class='card-body'>
  	<!-- Display the Title as a link to the Post's permalink. -->
 
- 	<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+ 	<h4><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
 
 
  	<!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
@@ -27,15 +25,17 @@
  	<!-- Display the Post's content in a div box. -->
 
  	<div class="entry">
- 		<?php the_content(); ?>
+ 		<?php the_excerpt(); ?>
  	</div>
-
+	
+	<a href=<?php the_permalink(); ?> class='btn btn-success'>Read More</a>
 
  	<!-- Display a comma separated list of the Post's Categories. -->
 
- 	<p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
+ 	<!-- <p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p> -->
  	</div> <!-- closes the first div box -->
-
+	
+	</div>
 
  	<!-- Stop The Loop (but note the "else:" - see next line). -->
 
@@ -49,5 +49,3 @@
 
  	<!-- REALLY stop The Loop. -->
  <?php endif; ?>
-
-<?php get_footer() ?>

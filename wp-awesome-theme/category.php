a603dfb1f7337d928a0de64d027f25d5bdf16711
alexxsanya <alexxsanya@gmail.com>
2020-03-02 20:32:18 +0300

    update layout

--- wp-awesome-theme/category.php
+++ wp-awesome-theme/category.php
@@ -1,30 +1,30 @@
 <?php get_header() ?>
 
-Categories Template
+<!-- Categories Template -->
 
 <div class='row'>
     <div class='col-lg-8'>
         <?php get_template_part('includes/section', 'archive') ?>
 
-		<!-- first ofrm of paginations -->
-		<!-- <?php previous_posts_link() ?>
-		<?php next_posts_link() ?> -->
+		<!-- first form of paginations of <prev next> -->
+		<?php previous_posts_link() ?>
+		<?php next_posts_link() ?>
 
-		<!-- send for of pagination -->
+		<!-- second form of pagination of 1,2 Next >> -->
 		<?php 
-			global $wp_query;
+			// global $wp_query;
 
-			$big = 9999999999; // need an unlikely number
-			echo paginate_links( array(
-				'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-				'format' => '?page=%#%',
-				'current' => max(1, get_query_var('paged')),
-				'total' => $wp_query->max_num_pages,
-			));
+			// $big = 9999999999; // need an unlikely number
+			// echo paginate_links( array(
+			// 	'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+			// 	'format' => '?page=%#%',
+			// 	'current' => max(1, get_query_var('paged')),
+			// 	'total' => $wp_query->max_num_pages,
+			// ));
 		?>
     </div>
     <div class='col-lg-4'>
-		<h4>Most Viewed </h4>
+		<h5>Most Viewed </h5>
 		<?php
 			//add list of posts based on the visits tracking
 			if (function_exists("awepop_popularity_list")) {

0aed13ee9fd159452d72bc79b74159a57449c986
alexxsanya <alexxsanya@gmail.com>
2020-03-02 20:18:15 +0300

    add custom plugin to count post views

--- wp-awesome-theme/category.php
+++ wp-awesome-theme/category.php
@@ -1,22 +1,38 @@
 <?php get_header() ?>
 
 Categories Template
-<?php get_template_part('includes/section', 'archive') ?>
 
-<!-- first ofrm of paginations -->
-<!-- <?php previous_posts_link() ?>
-<?php next_posts_link() ?> -->
+<div class='row'>
+    <div class='col-lg-8'>
+        <?php get_template_part('includes/section', 'archive') ?>
 
-<!-- send for of pagination -->
-<?php 
-	global $wp_query;
+		<!-- first ofrm of paginations -->
+		<!-- <?php previous_posts_link() ?>
+		<?php next_posts_link() ?> -->
 
-	$big = 9999999999; // need an unlikely number
-	echo paginate_links( array(
-		'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-		'format' => '?page=%#%',
-		'current' => max(1, get_query_var('paged')),
-		'total' => $wp_query->max_num_pages,
-	));
-?>
+		<!-- send for of pagination -->
+		<?php 
+			global $wp_query;
+
+			$big = 9999999999; // need an unlikely number
+			echo paginate_links( array(
+				'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+				'format' => '?page=%#%',
+				'current' => max(1, get_query_var('paged')),
+				'total' => $wp_query->max_num_pages,
+			));
+		?>
+    </div>
+    <div class='col-lg-4'>
+		<h4>Most Viewed </h4>
+		<?php
+			//add list of posts based on the visits tracking
+			if (function_exists("awepop_popularity_list")) {
+				awepop_popularity_list();
+			}
+		?>
+    </div>
+
+</div>
+
 <?php get_footer() ?>

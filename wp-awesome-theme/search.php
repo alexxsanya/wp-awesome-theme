3e3911b089ac2927cb86f456222962e81f6f0a42
alexxsanya <alexxsanya@gmail.com>
2020-03-08 00:24:05 +0300

    remove unneccessary code

--- wp-awesome-theme/search.php
+++ wp-awesome-theme/search.php
@@ -4,24 +4,11 @@
 
 <div class='row'>
     <div class='col-lg-8'>
-        <?php get_template_part('includes/section', 'archive') ?>
+        <?php get_template_part('includes/section', 'searchcontent') ?>
 
 		<!-- first form of paginations of <prev next> -->
 		<?php previous_posts_link() ?>
 		<?php next_posts_link() ?>
-
-		<!-- second form of pagination of 1,2 Next >> -->
-		<?php 
-			// global $wp_query;
-
-			// $big = 9999999999; // need an unlikely number
-			// echo paginate_links( array(
-			// 	'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-			// 	'format' => '?page=%#%',
-			// 	'current' => max(1, get_query_var('paged')),
-			// 	'total' => $wp_query->max_num_pages,
-			// ));
-		?>
     </div>
     <div class='col-lg-4'>
 	    <?php if(is_active_sidebar('blog_sidebar')):?>

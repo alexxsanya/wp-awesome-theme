d4cc479a6c0672f6560c20276691081665b185d8
alexxsanya <alexxsanya@gmail.com>
2020-03-07 23:34:22 +0300

    add widget support in the theme

--- wp-awesome-theme/functions.php
+++ wp-awesome-theme/functions.php
@@ -26,6 +26,7 @@ add_action( 'wp_enqueue_scripts', 'load_js' );
 //Theme Options
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
+add_theme_support('widgets');
 
 //Menus
 register_nav_menus(

a282e3123314b756d18b800b65975944b1624d0c
alexxsanya <alexxsanya@gmail.com>
2020-03-10 04:21:09 +0300

    add project template file

--- wp-awesome-theme/single-projects.php
+++ wp-awesome-theme/single-projects.php
@@ -1,10 +1,22 @@
 <?php get_header() ?>
+<h4><?php echo  get_post_meta($post->ID, 'project_name', true) ?></h4>
+<div class='row'>
+
+	<div class='col-lg-8'>
+		<?php if(has_post_thumbnail()):?> 
+			<img src=<?php the_post_thumbnail_url('blog-large')?>  alt="<?php the_title() ?>" class='img-fluid mb-3 img-thumbnail' />
+		<?php endif; ?>
+		<p><?php the_field('description') ?></p>
+	</div>
+
+	<div class='col-lg-4'>
+		<ul>
+			<!-- the_field(custom_field_name) is from acf plugin to replace get_post_meta -->
+			<li><?php the_field('project_name') ?></li>
+		</ul>
+	</div>
 
 
-<?php if(has_post_thumbnail()):?> 
-	<img src=<?php the_post_thumbnail_url('blog-large')?>  alt="<?php the_title() ?>" class='img-fluid mb-3 img-thumbnail' />
-<?php endif; ?>
-
-<?php get_template_part('includes/section', 'projects') ?>
+</div>
 
 <?php get_footer() ?>

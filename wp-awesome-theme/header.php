66717a118088e968031dd1e5045e39250d4008bf
alexxsanya <alexxsanya@gmail.com>
2020-03-13 13:39:28 +0300

    make menu items visible on mobile screens

--- wp-awesome-theme/header.php
+++ wp-awesome-theme/header.php
@@ -21,7 +21,7 @@
 			'theme_location'    => 'top-menu',
 			'depth'             => 2,
 			'container'         => 'div',
-			'container_class'   => 'collapse navbar-collapse',
+			// 'container_class'   => 'collapse navbar-collapse',
 			'container_id'      => 'bs-example-navbar-collapse-1',
 			'menu_class'        => 'nav navbar-nav',
 			'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',

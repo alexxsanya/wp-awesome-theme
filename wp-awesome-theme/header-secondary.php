c6f9bbfd83d6dfffca00b95658684faac160bd16
alexxsanya <alexxsanya@gmail.com>
2020-03-13 13:35:57 +0300

    add functional custom search

--- wp-awesome-theme/header-secondary.php
+++ wp-awesome-theme/header-secondary.php
@@ -28,11 +28,7 @@
 				'walker'          => new WP_Bootstrap_Navwalker(),
 			) );
 	?>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-	<?php //get_search_form(); ?>
+	<?php get_search_form(); ?>
   </div>
 </nav>
 
